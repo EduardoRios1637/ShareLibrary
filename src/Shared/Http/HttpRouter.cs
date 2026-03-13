ab46d1f5e16a58a305e8842cb273e5bf70dc8df6
riose <riose6304@gmail.com>
2026-03-13 14:51:46 -0400

    Add HttpUtils + JsonUtils

--- src/Shared/Http/HttpRouter.cs
+++ src/Shared/Http/HttpRouter.cs
@@ -55,8 +55,11 @@ public class HttpRouter
 		var req = ctx.Request;
 		var res = ctx.Response;
 		var props = new Hashtable();
+
 		res.StatusCode = RESPONSE_NOT_SENT;
+
 		props["req.id"] = ++requestId;
+
 		try
 		{
 			await HandleAsync(req, res, props, () => Task.CompletedTask);
@@ -67,21 +70,106 @@ public class HttpRouter
 			{
 				res.StatusCode = (int)HttpStatusCode.NotImplemented;
 			}
+
 			res.Close();
 		}
 	}
-	private async Task HandleAsync(HttpListenerRequest req,
-HttpListenerResponse res, Hashtable props, Func<Task> next)
+	private async Task HandleAsync(HttpListenerRequest req, HttpListenerResponse res, Hashtable props, Func<Task> next)
 	{
-		Func<Task> globalMiddlewarePipeline =
-		GenerateMiddlewarePipeline(req, res, props, middlewares);
+		Func<Task> globalMiddlewarePipeline = GenerateMiddlewarePipeline(req, res, props, middlewares);
 		await globalMiddlewarePipeline();
 		await next();
 	}
 	public HttpRouter UseRouter(string path, HttpRouter router)
 	{
 		router.basePath = this.basePath + path;
+
 		return Use(router.HandleAsync);
+	}
+
+	private Func<Task> GenerateMiddlewarePipeline(HttpListenerRequest req, HttpListenerResponse res, Hashtable props, List<HttpMiddleware> middlewares)
+	{
+		int index = -1;
+
+		Func<Task> next = () => Task.CompletedTask;
+
+		next = async () =>
+		{
+			index++;
+			if (index < middlewares.Count && res.StatusCode == RESPONSE_NOT_SENT)
+			{
+				await middlewares[index](req, res, props, next);
+			}
+		};
+		return next;
+	}
+	public HttpRouter UseSimpleRouteMatching()
+	{
+		return Use(SimpleRouteMatching);
+	}
+	public HttpRouter UseParametrizedRouteMatching()
+	{
+		return Use(ParametrizedRouteMatching);
+	}
+	private async Task SimpleRouteMatching(HttpListenerRequest req, HttpListenerResponse res, Hashtable props, Func<Task> next)
+	{
+		foreach (var (method, path, middlewares) in routes)
+		{
+			if (req.HttpMethod == method && string.Equals(req.Url!.AbsolutePath, basePath + path)) // *
+			{
+				Func<Task> routeMiddlewarePipeline = GenerateMiddlewarePipeline(req, res, props, middlewares.ToList());
+				await routeMiddlewarePipeline();
+				break; // or return; // To short-circuit global pipeline.
+			}
+		}
+
+		await next();
+	}
+	private async Task ParametrizedRouteMatching(HttpListenerRequest req, HttpListenerResponse res, Hashtable props, Func<Task> next)
+	{
+		foreach (var (method, path, middlewares) in routes)
+		{
+			NameValueCollection? parameters;
+
+			if (req.HttpMethod == method && (parameters = ParseUrlParams(req.Url!.AbsolutePath, basePath + path)) != null) // *
+			{
+
+				props["req.params"] = parameters;
+
+				Func<Task> routeMiddlewarePipeline = GenerateMiddlewarePipeline(req, res, props, middlewares.ToList());
+
+				await routeMiddlewarePipeline();
+				break; // or return; // To short-circuit global pipeline.
+			}
+		}
+
+		await next();
+	}
+	public static NameValueCollection? ParseUrlParams(string uPath, string rPath)
+	{
+		string[] uParts = uPath.Trim('/').Split('/', StringSplitOptions.RemoveEmptyEntries);
+		string[] rParts = rPath.Trim('/').Split('/', StringSplitOptions.RemoveEmptyEntries);
+
+		if (uParts.Length != rParts.Length) { return null; }
+
+		var parameters = new NameValueCollection();
+
+		for (int i = 0; i < rParts.Length; i++)
+		{
+			string uPart = uParts[i];
+			string rPart = rParts[i];
+
+			if (rPart.StartsWith(":"))
+			{
+				string paramName = rPart.Substring(1);
+				parameters[paramName] = HttpUtility.UrlDecode(uPart);
+			}
+			else if (uPart != rPart)
+			{
+				return null;
+			}
+		}
+		return parameters;
 	}
 
 }
